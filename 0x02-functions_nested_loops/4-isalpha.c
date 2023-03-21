99363942a33271e1a7ef90f80c8abae04da1017e
Anniefera1 <olatunjiopeyemi1206@yahoo.com>
2023-03-21 14:22:18 -0700

    git

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - checks for alphabetic character
+ *int _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
@@ -18,4 +18,4 @@ int _isalpha(int c)
 		}
 		_putchar('\n');
 	}
-
+}
