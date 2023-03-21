4f0521acfbd06973085b2b251835fd9da2e0e9b5
Anniefera1 <olatunjiopeyemi1206@yahoo.com>
2023-03-21 12:47:40 -0700

    main.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char letter1;
 	char letter2;
 
-	for (letter2 = 0; letter1 <= 10; letter1++)
+	for (letter2 = 1; letter1 <= 9; letter1++)
 	{
 		for (letter2 = 'a'; letter2 <= 'z'; letter2++)
 		{
